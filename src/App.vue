d041d9818e64e77d0e8edd2887f6bf14e7218269
Godzilla-Henry <koberidiculous@gmail.com>
2022-05-06 14:05:58 +0800

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,29 @@
 <template>
   <div id="app">
+    API : {{ api_data }}
+    <button @click="FetchApi()">Click</button>
+
+
   </div>
 </template>
 
 <script>
-
+import axios from 'axios';
 export default {
   name: 'App',
   data(){
     return{
+        api_data : []
     }
   },
+  methods:{
+    async FetchApi(){
+      await axios.get("https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json")
+      .then(res => {
+        console.log(res.data)
+      })
+    }
+  }
 }
 </script>
 
